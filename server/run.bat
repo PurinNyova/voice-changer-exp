56377b19ed607010e65ad3e37a12c32025724717
PurinNyova <FionaSherleen2005@outlook.com>
2026-05-02 02:08:54 +0700

    run.bat

--- server/run.bat
+++ server/run.bat
@@ -8,6 +8,7 @@ python MMVCServerSIO.py ^
   --content_vec_500_onnx pretrain/content_vec_500.onnx ^
   --content_vec_500_onnx_on true ^
   --hubert_base pretrain/hubert_base.pt ^
+  REM --light_hubert pretrain/lighthubert_base.pt ^
   --hubert_base_jp pretrain/rinna_hubert_base_jp.pt ^
   --hubert_soft pretrain/hubert/hubert-soft-0d54a1f4.pt ^
   --nsf_hifigan pretrain/nsf_hifigan/model ^
